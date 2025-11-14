d3a9e9937cbd832a426182aa7f81c7ed9afdb918
Daniel Chen <chendaniely@users.noreply.github.com>
2025-11-13 20:37:48 -0800

    formatting

--- regression1/code.py
+++ regression1/code.py
@@ -2,28 +2,6 @@ import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
-# scikit learn data comes in 2 parts
-# it separates the predictor data from response data
-skhouse = fetch_california_housing(as_frame=True)
-
-print(skhouse.DESCR)
-
-housing_x = skhouse["data"]
-housing_y = skhouse["target"]
-
-# split data!
-housing_x_train, housing_x_test, housing_y_train, housing_y_test = (
-    train_test_split(housing_x, housing_y, test_size=0.2, random_state=42)
-)
-
-# combine data to make it a single dataframe (for reference)
-housing = pd.concat([housing_x, housing_y], axis="columns")
-housing.columns
-housing.head()
-
-
-# --- model
-
 # import the K-NN regression model
 from sklearn.model_selection import GridSearchCV
 from sklearn.compose import make_column_transformer
@@ -34,23 +12,48 @@ from sklearn import set_config
 # import the K-NN regression model
 from sklearn.neighbors import KNeighborsRegressor
 
+# scikit learn data comes in 2 parts
+# it separates the predictor data from response data
+skhouse = fetch_california_housing(as_frame=True)
+
+print(skhouse.DESCR)
+
+housing_x = skhouse["data"]
+housing_y = skhouse["target"]
+
+# split data!
+housing_x_train, housing_x_test, housing_y_train, housing_y_test = train_test_split(
+    housing_x, housing_y, test_size=0.2, random_state=42
+)
+
+# combine data to make it a single dataframe (for reference)
+housing = pd.concat([housing_x, housing_y], axis="columns")
+housing.columns
+housing.head()
+
+
+# --- model
+
+
 # Output dataframes instead of arrays
 set_config(transform_output="pandas")
 
 # preprocess the data, make the pipeline
-preprocessor = make_column_transformer((
-    StandardScaler(),
-    [
-        "MedInc",
-        "HouseAge",
-        "AveRooms",
-        "AveBedrms",
-        "Population",
-        "AveOccup",
-        "Latitude",
-        "Longitude",
-    ],
-))
+preprocessor = make_column_transformer(
+    (
+        StandardScaler(),
+        [
+            "MedInc",
+            "HouseAge",
+            "AveRooms",
+            "AveBedrms",
+            "Population",
+            "AveOccup",
+            "Latitude",
+            "Longitude",
+        ],
+    )
+)
 pipeline = make_pipeline(preprocessor, KNeighborsRegressor())
 
 # create the 5-fold GridSearchCV object
