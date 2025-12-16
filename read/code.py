3aeae4816b52f420ece374b2d39f33cd8a6ef677
Daniel Chen <chendaniely@users.noreply.github.com>
2025-12-15 16:38:37 -0800

    updates from OH

--- read/code.py
+++ read/code.py
@@ -1,7 +1,7 @@
 ## DO NOT RUN
 import os
 
-os.chdir("read/")
+# os.chdir("read/")
 ##
 
 
@@ -11,7 +11,12 @@ pd.read_csv("data/data1.csv")
 
 pd.read_csv("data-meta/data2.csv")
 pd.read_csv("data-meta/data2.csv", skiprows=5)
-
+pd.read_csv("data-meta/data2.csv", skiprows=6)
+pd.read_csv("data-meta/data2.csv", skiprows=7)
 
 pd.read_csv("data-meta-delim/data3.csv", skiprows=5)
 pd.read_csv("data-meta-delim/data3.csv", skiprows=5, delimiter="$")
+pd.read_csv("data-meta-delim/data3.csv", skiprows=5, sep="$")
+
+pd.read_csv("data/data1.csv", skiprows=0, sep=",")
+pd.read_csv("data3-same.csv", skiprows=5, sep="$")
--- tidy/code.py
+++ tidy/code.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
 URL = "https://raw.githubusercontent.com/cmrivers/ebola/refs/heads/master/country_timeseries.csv"
-
 ebola = pd.read_csv(URL)
-
 ebola
 
 ebola_long = ebola.melt(
-    id_vars=["Date", "Day"], var_name="cd_country", value_name="count"
+    id_vars=["Date", "Day"],
+    var_name="cd_country",
+    value_name="count",
 )
 ebola_long
 
 
 # don't need to know
 ebola_long[["case_death", "country"]] = ebola_long.cd_country.str.split(
-    "_", expand=True
+    "_",
+    expand=True,
 )
 ebola_long
 
@@ -24,8 +25,9 @@ ebola_long.groupby(["case_death", "country"])["count"].max().reset_index()
 
 ebola_tidy = (
     ebola_long.drop(columns="cd_country")  # dropping a column
-    .pivot_table(index=["Date", "Day", "country"], columns="case_death", values="count")
+    .pivot(index=["Date", "Day", "country"], columns="case_death", values="count")
     .reset_index()
+    .dropna()
 )
 ebola_tidy
 
